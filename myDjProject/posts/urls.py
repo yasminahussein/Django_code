06c730f31e13e48c2d7f6e488c08bdce7f6e5667
Yasmina Hussein <yasminaaahussien@gmail.com>
2024-08-25 14:46:29 +0300

    create Slug

--- myDjProject/posts/urls.py
+++ myDjProject/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views 
 '''. ==> mean from the current dir we in'''
 
+app_name="posts"
+
 urlpatterns = [
-    path('', views.posts_list),
+    path('', views.posts_list , name="list"),
+    path('<slug:slug>',views.post_page,name="page"),
     
 ]
